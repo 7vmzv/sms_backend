47470fea581ed2e0a8f4a067c06370264421dec4
7vmzv <rbatihamza@hotmail.com>
2022-10-04 15:08:29 +0200

    database initialisation function

--- src/main/java/com/eil/sms_backend/SmsBackendApplication.java
+++ src/main/java/com/eil/sms_backend/SmsBackendApplication.java
@@ -1,7 +1,13 @@
 package com.eil.sms_backend;
 
+import com.eil.sms_backend.entities.Product;
+import com.eil.sms_backend.services.ProductService;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import java.util.Date;
 
 @SpringBootApplication
 public class SmsBackendApplication {
@@ -10,4 +16,14 @@ public class SmsBackendApplication {
 		SpringApplication.run(SmsBackendApplication.class, args);
 	}
 
+	@Bean
+	CommandLineRunner start(ProductService productService) {
+		return (args -> {
+			for(int i = 0; i < 10; i++) {
+				productService.addProduct(
+						new Product(null, "code------"+i, "product " + i, Math.random() * 10, new Date())
+				);
+			}
+		});
+	}
 }
